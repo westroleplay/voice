cf5107675ea19be623688441ef13debf3ca58810
Zerio <neoboij@gmail.com>
2024-07-22 22:11:27 +0200

    chore: change fxmanifest game to rdr3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,7 @@
-game("gta5")
+game("rdr3")
 fx_version("cerulean")
+rdr3_warning(
+'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.')
 
 version("1.0.0")
 author("Zerio")
