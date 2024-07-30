772b9a24a621897348b2a644b1d17bf75b9be79e
Zerio <neoboij@gmail.com>
2024-07-30 03:01:25 +0200

    chore: remove unneeded logic

--- apps/client/src/modules/nui.ts
+++ apps/client/src/modules/nui.ts
@@ -1,22 +1,7 @@
 import { NuiCallback } from "@zerio-voice/utils/structs";
 
-const offsetX = 100;
-const offsetY = 50;
-
 class Nui {
-  hasFocus = false;
-
   constructor() {
-    RegisterNuiCallback("removeFocus", (_data: unknown, cb: NuiCallback) => {
-      this.toggleFocus(false);
-
-      SendNUIMessage({
-        action: "closed"
-      });
-
-      cb("ok");
-    });
-
     RegisterNuiCallback("load", (_data: unknown, cb: NuiCallback) => {
       SendNUIMessage({
         action: "updateDebugState",
@@ -25,26 +10,6 @@ class Nui {
 
       cb("ok");
     });
-  }
-
-  getInitialCoords(): [number, number] {
-    const [resX, resY] = GetActiveScreenResolution();
-
-    return [(resX - offsetX) / resX, (resY - offsetY) / resY];
-  }
-
-  toggleFocus(toggled: boolean) {
-    if (this.hasFocus !== toggled) {
-      this.hasFocus = toggled;
-
-      if (toggled) {
-        const initialCoords = this.getInitialCoords();
-
-        SetCursorLocation(initialCoords[0], initialCoords[1]);
-      }
-
-      SetNuiFocus(toggled, toggled);
-    }
   }
 }
 
